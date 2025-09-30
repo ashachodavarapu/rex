ba6b86f8cd622b0ebf34c2368daae7cdd22a069d
YourPersonalName <asha.chodavarapu@infor.com>
2025-09-30 21:20:23 +0530

    Added mobile swipe functionality to carousel with touch gestures, hidden arrows on mobile for shopping app experience

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,6 +23,8 @@ import { FooterComponent } from './footer/footer.component';
 export class AppComponent {
   title = 'rex_bussines';
   currentSection = 'home';
+  private touchStartX = 0;
+  private touchEndX = 0;
   
   navigateToSection(section: string) {
     this.currentSection = section;
@@ -40,5 +42,29 @@ export class AppComponent {
     const currentIndex = sections.indexOf(this.currentSection);
     const prevIndex = currentIndex === 0 ? sections.length - 1 : currentIndex - 1;
     this.currentSection = sections[prevIndex];
+  }
+  
+  onTouchStart(event: TouchEvent) {
+    this.touchStartX = event.changedTouches[0].screenX;
+  }
+  
+  onTouchEnd(event: TouchEvent) {
+    this.touchEndX = event.changedTouches[0].screenX;
+    this.handleSwipe();
+  }
+  
+  private handleSwipe() {
+    const swipeThreshold = 50;
+    const diff = this.touchStartX - this.touchEndX;
+    
+    if (Math.abs(diff) > swipeThreshold) {
+      if (diff > 0) {
+        // Swipe left - next slide
+        this.nextSlide();
+      } else {
+        // Swipe right - previous slide
+        this.previousSlide();
+      }
+    }
   }
 }
